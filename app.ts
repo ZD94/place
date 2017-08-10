2bcb85f498175705f1ac3fb5bf35cf5f2e54fedf
zhangdong <dong.zhang@jingli365.com>
2017-08-10 10:44:01 +0800

    接口设计

--- app.ts
+++ app.ts
@@ -9,8 +9,9 @@ import express = require("express");
 import {type} from "os";
 const app = express();
 import {DB} from '@jingli/database';
+import _ = require("lodash")
 
-
+//城市信息
 app.get("/city/:id", async (req, res, next) => {
     try {
         let {id} = req.params;
@@ -19,18 +20,97 @@ app.get("/city/:id", async (req, res, next) => {
             lang = 'zh';
         }
         let city = await DB.models['City'].findById(id);
-        let alternateName = await DB.models['CityAlterName'].findOne({where: {cityId: id, lang: lang}});
+        let ret = city.toJSON();
+        let alternateNames = await DB.models['CityAltName'].find({where: {cityId: id}});
+        let alternateName = await DB.models['CityAltName'].findOne({where: {cityId: id, lang: lang}});
         if (alternateName) {
-            city.name = alternateName.value;
+            ret.name = alternateName.value;
         }
-        res.json(city);
-    } catch(err) {
+        ret.alternateNames = alternateNames;
+        res.json(ret);
+    } catch (err) {
         next(err);
     }
 });
 
+//城市下一级
+app.get("/city/:id/children", async (req, res, next) => {
+    try {
+        let {id} = req.params;
+        let city = await DB.models['City'].findAll();
+        let result = [];
+        city.map((cities) => {
+            if (cities.parentId == id) {
+                result.push(cities)
+            }
+        });
+        if (result.length > 0) {
+            res.json(result)
+        } else {
+            res.send('该城市没有下一级')
+        }
+    } catch (err) {
+        next(err);
+    }
+});
+
+//城市上一级
+app.get('/city/:id/parent', async (req, res, next) => {
+    try {
+        let {id} = req.params;
+        let {lang} = req.query;
+        if (!lang) {
+            lang = 'zh';
+        }
+        let city = await DB.models['City'].findById(id);
+        let parentCity = await DB.models['City'].findById(city.parentId);
+        if (parentCity) {
+            let ret = parentCity.toJSON();
+            let alternateNames = await DB.models['CityAltName'].find({where: {cityId: id}});
+            let alternateName = await DB.models['CityAltName'].findOne({where: {cityId: id, lang: lang}});
+            if (alternateName) {
+                ret.name = alternateName.value;
+            }
+            ret.alternateNames = alternateNames;
+            res.json(ret)
+        } else {
+            res.send('该城市没有上一级')
+        }
+    } catch (err) {
+        next(err)
+    }
+});
+
+//城市全部别名
+app.get('/city/:id/altername', async (req, res, next) => {
+    try {
+        let {id} = req.params;
+        let alternateName = await DB.models['CityAltName'].findAll({where: {cityId: id}});
+        // let alterName =alternateName.value
+        let alterName = [];
+        alternateName.map((alternateName) => {
+            alterName.push(alternateName.value)
+        });
+        res.json(alterName)
+    } catch (err) {
+        next(err)
+    }
+});
+
+//城市别名
+app.get('/city/:id/altername/:lang', async (req, res, next) => {
+    try {
+        let {id} = req.params;
+        let {lang} = req.params;
+        let alternateName = await DB.models['CityAltName'].find({where: {cityId: id, lang: lang}});
+        res.json(alternateName.value)
+    } catch (err) {
+        next(err)
+    }
+})
+
 app.get('/test', (req, res, next) => {
     res.send('ok');
-})
+});
 
 export default app;
