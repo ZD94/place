ded83491550cfe6804e4c2d355bb52549c09dc3d
lisongwei <1429594204@qq.com>
2017-12-12 09:47:27 +0800

    .

--- app.ts
+++ app.ts
@@ -7,18 +7,25 @@
 import path = require("path");
 import express = require("express");
 const app = express();
-import {registerControllerToRouter, scannerDecoration} from '@jingli/restful';
+import { registerControllerToRouter, scannerDecoration } from '@jingli/restful';
+const bodyParser = require('body-parser')
 
 // import {DB} from '@jingli/database';
 // import _ = require("lodash")
-
-app.use('/manager', function (req, res, next) { 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/manager', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    if (req.query.key !== 'test1512967479893') { 
-        return res.send(404);
+    res.header('Access-Control-Allow-Methods', 'PUT,GET,POST,OPTIONS,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    if (req.method.toLowerCase() == 'options')
+        return res.sendStatus(200)
+    if (req.query.key !== 'test1512967479893') {
+        return res.send(403);
     }
     next();
 })
+
 scannerDecoration(path.join(__dirname, 'controller'));
 registerControllerToRouter(app, {
     isShowUrls: true,
